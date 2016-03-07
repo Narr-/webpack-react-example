9b1598f1c4c502a7ecaa0bcc441e7d4fc589f01e
Narr <nardgu@gmail.com>
2016-03-08 07:33:11 +0900

    travis test

--- server/models/index.js
+++ server/models/index.js
@@ -10,13 +10,17 @@ export const pgNative = pg.native;
 // it will keep idle connections open for a (configurable) 30 seconds
 // and set a limit of 20 (also configurable)
 pgNative.connect(POSTGRESQL_URI, (err, client, done) => {
+  console.log('pgNative..!! pgNative..!! pgNative..!! pgNative..!!');
   if (err) {
+    console.log('pgNative2..!! pgNative2..!! pgNative2..!! pgNative2..!!');
     return logger.error(`error fetching client from pool: ${err}`);
   }
   client.query(CREATE_TODO_TABLE, (queryErr) => {
     // call `done()` to release the client back to the pool
+    console.log('pgNative3..!! pgNative3..!! pgNative3..!! pgNative3..!!');
     done();
     if (queryErr) {
+      console.log('pgNative4..!! pgNative4..!! pgNative4..!! pgNative4..!!');
       return logger.error(`error running query ${queryErr}`);
     }
   });
