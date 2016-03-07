63f9a6d5f677ccf2af4058a9b8d6056c03736906
Narr <nardgu@gmail.com>
2016-03-08 05:43:06 +0900

    travis test

--- server/config/index.js
+++ server/config/index.js
@@ -11,10 +11,8 @@ if (NODE_ENV === 'docker') {
   POSTGRESQL_URI = env.DATABASE_URL;
   REDIS_URL = env.REDIS_URL;
 } else {
-  POSTGRESQL_URI = `postgres://${env.POSTGRES_ENV_POSTGRES_USER}@postgres:5432/${env.WRE_POSTGRES_ENV_POSTGRES_DB}`;
-  REDIS_URL = 'redis://redis:6379';
-  // POSTGRESQL_URI = 'postgres://narr@192.168.99.100:5433/todo_db';
-  // REDIS_URL = 'redis://192.168.99.100:6379';
+  POSTGRESQL_URI = 'postgres://narr@192.168.99.100:5433/todo_db';
+  REDIS_URL = 'redis://192.168.99.100:6379';
 }
 
 export default {
